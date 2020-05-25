b272da12a8791379a6fb7ede91fe5b6a35f7528a
SOARINDEN\soarin <simbatlk@gmail.com>
2020-05-25 19:35:11 +0300

    Initial commit

---
+++ Tests/Controllers/LogTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using WebApplication.Controllers; 
+
+namespace Tests.Controllers
+{
+    public class LogTest
+    {
+        private LogController _logController;
+
+        [SetUp]
+        public void Setup()
+        {
+            var logger = new NullLogger<LogController>();
+            this._logController = new LogController(logger);
+        }
+
+        [Test]
+        public void TestLogActionReturnsOkWhenAdUnitIdIsPassed()
+        {
+            Assert.IsInstanceOf(typeof(OkObjectResult), _logController.Get(123));
+        }
+        
+        [Test]
+        public void TestLogActionReturnsBadRequestWhenAdUnitIdIsNotPassed()
+        {
+            Assert.IsInstanceOf(typeof(BadRequestObjectResult), _logController.Get());
+        }
+    }
+}
---
+++ WebApplication/Controllers/LogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] int adUnitId = 0)
+        {
+            if (adUnitId != 0)
+            {
+                return new OkObjectResult("Okey");
+            }
+
+            return new BadRequestObjectResult("You positively must pass an ID");
+        }
+    }
+}
